f3eab7735db3de93494dca3638de7df5ddd51a1b
Maikel W <maikelweesenaar@gmail.com>
2018-05-09 12:35:05 +0200

    Added code for accessing REST

---
+++ BluckurWallet/BluckurWallet/Domain/QuizReply.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace BluckurWallet.Domain
+{
+	[JsonObject]
+    public class QuizReply
+    {
+		[JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("correct")]
+        public bool IsCorrect { get; set; }
+
+		[JsonProperty("correctAnswer")]
+        public string CorrectAnswer { get; set; }
+
+		[JsonProperty("secret")]
+		public string Secret { get; set; }
+
+		public QuizReply(){}
+
+		public QuizReply(int id, bool isCorrect, string correctAnswer, string secret)
+		{
+			this.Id = id;
+			this.IsCorrect = isCorrect;
+			this.CorrectAnswer = correctAnswer;
+			this.Secret = secret;
+		}
+    }
+}
